95a5b3be3c22e4e3e3391d3d84be35c25745e871
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-28 21:14:17 +0100

    Included the 'include' function for representing the module urls.

--- hospital/urls.py
+++ hospital/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home.views import index
 
 urlpatterns = [
-    path ('', index, name='home'),
-    #path ('home/', index, name='home'),
+    path ('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
