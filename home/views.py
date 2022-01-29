155364d0f40a1e278399c3589c97295049e75d50
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-29 22:58:59 +0100

    Test code.

--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
 import imp
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from patient.models import Patient
 
 # Create your views here.
 
 
 def index(request):
+    patient = Patient.objects.all()
     context = {
-
+        'patient': patient
     }
     return render(request, 'home.html', context)
