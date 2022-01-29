3a32849ddd4211a5cd6b785cf81a9cb032f437b6
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-29 19:25:31 +0100

    Registered the Patient model in Admin panel. Modified the admin model fields display.

--- patient/admin.py
+++ patient/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Patient
+
 
 # Register your models here.
+
+
+class PatientAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="auto" height="50px" />'.format(obj.image.url))
+    
+    image_tag.short_description = 'Image Preview'
+    image_tag.allow_tags = True
+
+    TEXT = "Click on to 'hide' to hide this section."
+
+    list_display = ['name', 'surname', 'middlename', 'patient_ID', 'age', 'created_at', 'updated_at', 'image_tag']
+    list_filter = ['name', 'surname', 'patient_ID']
+    fieldsets = (
+        ('Basic Info', {
+            'fields': [('name', 'middlename', 'surname',), 'age', 'patient_ID'],
+        }),
+        ('Contact Info', {
+            'fields': [('phone', 'mobile', 'email')],
+            'classes': ('collapse',),
+            'description': '%s' % TEXT,
+        }),
+        ('Address Info', {
+            'fields': [('address', 'city', 'country')],
+            'classes': ('collapse',),
+            'description': '%s' % TEXT,
+        }),
+        ('Image', {
+            'fields': ['image', 'image_tag'],
+            'classes': ('collapse',),
+            'description': '%s' % TEXT,
+        }),
+    )
+    readonly_fields = ['image_tag']
+
+
+admin.site.register(Patient, PatientAdmin)
