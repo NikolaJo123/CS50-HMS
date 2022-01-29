5f9a2be42813641bc766a283ee1792e77fbe2c8c
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-29 19:20:55 +0100

    Added media root and url

--- hospital/settings.py
+++ hospital/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
