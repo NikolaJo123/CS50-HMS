0e91ba014503200c80f5c925910a03a031cd40ef
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-30 20:53:55 +0100

    Registered the hospital staff module in 'installed_apps'.

--- hospital/settings.py
+++ hospital/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'patient.apps.PatientConfig',
+    'hospital_staff.apps.HospitalStaffConfig',
+    'mptt',
 ]
 
 MIDDLEWARE = [
