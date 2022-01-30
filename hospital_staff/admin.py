b284e2b813a00d5daa064ff93fd4e76b9b15158c
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-30 23:43:42 +0100

    Changed the modelAdmin with DraggableMPTTAdmin.

--- hospital_staff/admin.py
+++ hospital_staff/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from mptt.admin import DraggableMPTTAdmin
 
 from .models import Hospital_Staff
 
@@ -7,9 +8,13 @@ from .models import Hospital_Staff
 # Register your models here.
 
 
-class HospitalStaffAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at', 'updated_at']
+class HospitalStaffAdmin(DraggableMPTTAdmin):
+    mptt_indent_field = 'title'
+    #list_display = ['title', 'created_at', 'updated_at']
+    list_display = ('tree_actions', 'indented_title')
+    list_display_links = ('indented_title',)
     list_filter = ['title']
+    prepopulated_fields = {'keywords': ('title',)}
 
 
 admin.site.register(Hospital_Staff, HospitalStaffAdmin)
