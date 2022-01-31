d7641ddb6c8ca565ce11884e98031dc9a068301b
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-31 14:45:50 +0100

    Registered the employee model in Django Admin.

--- hospital_staff/admin.py
+++ hospital_staff/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from mptt.admin import DraggableMPTTAdmin
 
-from .models import Hospital_Staff
+from .models import Hospital_Staff, Employee
 
 
 # Register your models here.
@@ -10,11 +10,23 @@ from .models import Hospital_Staff
 
 class HospitalStaffAdmin(DraggableMPTTAdmin):
     mptt_indent_field = 'title'
-    #list_display = ['title', 'created_at', 'updated_at']
     list_display = ('tree_actions', 'indented_title')
     list_display_links = ('indented_title',)
     list_filter = ['title']
     prepopulated_fields = {'keywords': ('title',)}
 
 
+class EmployeeAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="auto" height="50px" />'.format(obj.image.url))
+    
+    image_tag.short_description = 'Image Preview'
+    image_tag.allow_tags = True
+
+    list_display = ['name', 'surname', 'middlename', 'personal_ID_number', 'employed', 'updated_info', 'image_tag']
+    list_filter = ['name', 'surname', 'personal_ID_number']
+    readonly_fields = ['image_tag']
+
+
 admin.site.register(Hospital_Staff, HospitalStaffAdmin)
+admin.site.register(Employee, EmployeeAdmin)
