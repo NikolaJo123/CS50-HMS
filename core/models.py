e4c58734f1df71213a1ced1a2bfcc663dcaea982
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-01 18:55:29 +0100

    Created core models for location user-data and user-contact.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Location(models.Model):
+    address = models.CharField(blank=True, max_length=200)
+    city = models.CharField(blank=True, max_length=50)
+    country = models.CharField(blank=True, max_length=50)
+
+
+class UserContact(models.Model):
+    phone = models.CharField(max_length=30, blank=True, null=True)
+    mobile = models.CharField(max_length=30, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+
+class UserData(models.Model):
+    name = models.CharField(max_length=30)  #
+    surname = models.CharField(max_length=30)
+    middlename = models.CharField(max_length=30, blank=True, null=True)
+    birthdate = models.DateField('birthdate', blank=True, null=True, auto_now=False, auto_now_add=False)
+    user_image = models.ImageField(blank=True, upload_to='images/')
+
