1d77078c0c16561dccef3965cba65a5a6de211b7
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-02 22:46:28 +0100

    Registered the appointment model in admin.

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Appointment
+
 
 # Register your models here.
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+
+    list_display = ['patient', 'patient_personal_ID', 'clinic', 'doctor', 'appointed_by']
+    list_filter = ['patient', 'patient_personal_ID', 'clinic', 'doctor', 'appointed_by']
+    
+
+
+admin.site.register(Appointment, AppointmentAdmin)
