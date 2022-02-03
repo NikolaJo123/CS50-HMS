4b1ff19d70d9824d7d0786b0249b251fcd159e51
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-03 18:49:46 +0100

    Deleted the '.apps.AppnameConfig' at the app componenets in installed apps.

--- hospital/settings.py
+++ hospital/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'patient.apps.PatientConfig',
-    'hospital_staff.apps.HospitalStaffConfig',
-    'appointment.apps.AppointmentConfig',
+    'patient',
+    'hospital_staff',
+    'appointment',
     'core',
     'mptt',
 ]
