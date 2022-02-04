ea6f207db9156daf64c5cd07879177a4306e93ff
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-04 17:12:07 +0100

    Added API url for the patient view.

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from home.views import index
 from django.urls import path, include
+
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('patient/', views.patient, name='patient'),
 ]
