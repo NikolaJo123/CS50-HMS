1dcc578d2aace150110847d74bdad8f2645657cf
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-04 17:13:46 +0100

    Created fetch function.

--- home/static/hms/index.js
+++ home/static/hms/index.js
@@ -7,9 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function test_fetch() {
-    fetch(`https://api.exchangerate.host/latest`)
+    //fetch(`https://api.exchangerate.host/latest`)
+    fetch(`patient/`)
     .then(response => response.json())
-    .then(profile => {
-        console.log(profile)
+    .then(patients => {
+        console.log(patients)
+
+        for (let i of Object.keys(patients)) {
+            const patient = document.createElement('div')
+            patient.classList.add('patient')
+
+            patient.innerHTML = `
+                <div>${patients[i].name}</div>
+                <div>${patients[i].surname}</div>
+                <div>${patients[i].middlename}</div>
+                <div>${patients[i].patient_ID}</div>
+                <div>${patients[i].age}</div>
+                <div>${patients[i].phone}</div>
+                <div>${patients[i].mobile}</div>
+                <div>${patients[i].email}</div>
+                <div>${patients[i].address}</div>
+                <div>${patients[i].city}</div>
+                <div>${patients[i].country}</div>
+            `;
+
+            document.querySelector('#patientview').append(patient);
+        };
     });
 }
