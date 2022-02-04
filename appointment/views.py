6eb4f8a050bcc5d01faddc6d9b914550becd8187
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-04 13:05:39 +0100

    Created appointment view.

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 # Create your views here.
+
+
+def appointment(request):
+    return HttpResponse('Hello World!')
