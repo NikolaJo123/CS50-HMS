5ce20c9336debeb7234809a81c2e3c9d284da05c
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-05 17:34:57 +0100

    Registered the department model in django admin.

--- clinics/admin.py
+++ clinics/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Department
+
 
 # Register your models here.
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['department_name', 'created_at', 'updated_at']
+    list_filter = ['department_name']
+    prepopulated_fields = {'keywords': ('department_name',)}
+
+
+admin.site.register(Department, DepartmentAdmin)
