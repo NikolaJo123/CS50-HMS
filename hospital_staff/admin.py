baadbe9eaa858e9cf311a01a26276f5edebec12a
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-05 15:02:26 +0100

    Registerde the clinics model in admin.

--- hospital_staff/admin.py
+++ hospital_staff/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from mptt.admin import DraggableMPTTAdmin
 
-from .models import Staff, Employee
+from .models import Staff, Employee, Clinic
 
 
 # Register your models here.
@@ -25,14 +25,14 @@ class EmployeeAdmin(admin.ModelAdmin):
 
     TEXT = "Click on to 'hide' to hide this section."
 
-    list_display = ['user', 'role', 'employed', 'updated_info', 'image_tag']
+    list_display = ['user', 'role', 'speciality', 'employed', 'updated_info', 'image_tag']
     list_filter = ['user', 'role']
     fieldsets = (
         ('Basic Info', {
             'fields': [('user', 'middlename',), 'birthdate', 'personal_ID_number'],
         }),
         ('Status', {
-            'fields': [('role', 'status')]
+            'fields': [('role', 'speciality', 'status')]
         }),
         ('Contact Info', {
             'fields': [('phone', 'mobile', 'email')],
@@ -53,5 +53,13 @@ class EmployeeAdmin(admin.ModelAdmin):
     readonly_fields = ['image_tag']
 
 
+class ClinicAdmin(admin.ModelAdmin):
+    list_display = ['department', 'created_at', 'updated_at']
+    list_filter = ['department']
+    prepopulated_fields = {'keywords': ('department',)}
+
+
+
 admin.site.register(Staff, StaffAdmin)
 admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Clinic, ClinicAdmin)
