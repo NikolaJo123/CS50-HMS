d888e3a365a5b5e14616852f2869fd8b6432d08a
NikolaJo123 <nikola_ATE@hotmail.com>
2022-02-05 17:34:32 +0100

    Created Department model.

--- clinics/models.py
+++ clinics/models.py
@@ -1,3 +1,16 @@
+from turtle import title
 from django.db import models
 
+
 # Create your models here.
+
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=25)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.department_name
