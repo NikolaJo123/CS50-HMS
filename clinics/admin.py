02c579c3593f81b142b08cb303844089243639fe
NikolaJo123 <nikola_ate@hotmail.com>
2022-02-20 01:57:12 +0100

    Modified the admin panel for the new mptt model.

--- clinics/admin.py
+++ clinics/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from mptt.admin import DraggableMPTTAdmin
 
 from .models import Department
 
@@ -7,8 +8,10 @@ from .models import Department
 # Register your models here.
 
 
-class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ['department_name', 'created_at', 'updated_at']
+class DepartmentAdmin(DraggableMPTTAdmin):
+    mptt_indent_field = 'department_name'
+    list_display = ('tree_actions', 'indented_title', 'department_name', 'created_at', 'updated_at')
+    list_display_links = ('indented_title',)
     list_filter = ['department_name']
     prepopulated_fields = {'keywords': ('department_name',)}
 
