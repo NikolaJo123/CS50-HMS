20fa7d583cf85f13193b64674fbd4e4449c2ac5b
NikolaJo123 <nikola_ate@hotmail.com>
2022-02-21 20:08:10 +0100

    Adde urls for user authentication methods.

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login_view, name="login"),
+    path('register/', views.register, name="register"),
+    path("logout/", views.logout_view, name="logout"),
 
     # API Routes
     path('patients/patient/', views.patient, name='patient'),
