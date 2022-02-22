df7d901c66c5b975c85cfb733a35822ce8d4ad39
NikolaJo123 <nikola_ate@hotmail.com>
2022-02-22 15:36:31 +0100

    Created decorator file and autehnticated user restrictions method.

---
+++ core/decorators.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
+
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        else:
+            return view_func(request, *args, **kwargs)
+    
+    return wrapper_func
+
+            
