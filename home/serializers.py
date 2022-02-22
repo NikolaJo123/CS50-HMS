c4b45cc780cfe20d168e4b1498c522f6420afbb2
NikolaJo123 <nikola_ate@hotmail.com>
2022-02-22 19:10:59 +0100

    Deleted the employee staff component serizlizers.

--- home/serializers.py
+++ home/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from clinics.models import Department
-from hospital_staff.models import Speciality, Employee, Staff
 
 
 
@@ -9,33 +8,4 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = ('id', 'department_name', 'description', 'created_at', 'updated_at')
-
-
-class SpecialitySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Speciality
-        fields = ('id', 'department', 'keywords', 'description', 'created_at', 'updated_at')
-
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    employee_name = serializers.CharField(source='user.username')
-    employee_surname = serializers.CharField(source='user.last_name')
-    role = serializers.CharField(source='role.title')
-    speciality = serializers.CharField(source='speciality.department')
-
-    class Meta:
-        model = Employee
-        fields = ('id', 'user', 'employee_name', 'employee_surname', 'middlename', 
-                'role', 'speciality', 'personal_ID_number', 'status', 'birthdate',
-                'user_image', 'employed', 'updated_info')
-
-
-class StaffSerializer(serializers.ModelSerializer):
-    parent_name = serializers.CharField(source='parent')
-
-    class Meta:
-        model = Staff
-        fields = ('id', 'parent', 'parent_name', 'title', 'keywords', 'description',
-                'created_at', 'updated_at')
 
