1904548a92b5399011ffe886dd511bc583ed2ab5
NikolaJo123 <nikola_ate@hotmail.com>
2022-02-22 15:37:39 +0100

    Changed the user authentication restrictions restrictions using decorator from 'core.decortors'.

--- home/views.py
+++ home/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from core.decorators import unauthenticated_user
 from django.contrib.auth.models import User
 
 from .forms import LoginForm, SignUpForm
@@ -38,31 +39,26 @@ def index(request):
         return HttpResponseRedirect(reverse("login"))
 
 
+@unauthenticated_user
 def login_view(request):
     form = LoginForm(request.POST or None)
     msg = None
-    current_user = request.user
 
+    if request.method == "POST":
 
-    if current_user.is_authenticated:
-        return HttpResponseRedirect('/')
-    else:
-
-        if request.method == "POST":
-
-            if form.is_valid():
-                username = form.cleaned_data.get("username")
-                password = form.cleaned_data.get("password")
-                user = authenticate(username=username, password=password)
-                if user is not None:
-                    login(request, user)
-                    return redirect("/")
-                else:
-                    msg = 'Invalid credentials'
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
             else:
-                msg = 'Error validating the form'
+                msg = 'Invalid credentials'
+        else:
+            msg = 'Error validating the form'
 
-        return render(request, "accounts/login.html", {"form": form, "msg": msg})
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
 
 def logout_view(request):
@@ -70,35 +66,31 @@ def logout_view(request):
     return HttpResponseRedirect(reverse("index"))
 
 
+@unauthenticated_user
 def register(request):
     msg = None
     success = False
-    current_user = request.user
 
-    if current_user.is_authenticated:
-        return HttpResponseRedirect('/')
-    else:
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
 
-        if request.method == "POST":
-            form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
 
-            if form.is_valid():
-                form.save()
-                username = form.cleaned_data.get("username")
-                raw_password = form.cleaned_data.get("password1")
-                user = authenticate(username=username, password=raw_password)
-                login(request, user)
+            msg = 'User created successfully!'
+            success = True
 
-                msg = 'User created successfully!'
-                success = True
-
-                return HttpResponseRedirect('/')
-            else:
-                msg = 'Invalid Credentials!'
+            return HttpResponseRedirect('/')
         else:
-            form = SignUpForm()
-        
-        return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+            msg = 'Invalid Credentials!'
+    else:
+        form = SignUpForm()
+    
+    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
 
 
 def patients(request):
