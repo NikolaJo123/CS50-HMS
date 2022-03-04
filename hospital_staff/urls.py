7d2a0bde553e00c5aaff4d2759752061d44871ac
NikolaJo123 <nikola_ate@hotmail.com>
2022-03-04 11:37:11 +0100

    Creadet urls for the staff api's and the staff index view.

---
+++ hospital_staff/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('medicalstaff/', views.medicalstaff, name='medicalstaff'),
+    path('profile/', views.user_profile, name='user_profile'),
+
+    # API Routes
+    path('getprofile/', views.getprofile, name='getprofile'),
+    path('getpersonal/', views.getpersonal, name='getpersonal'),
+    path('employees/', views.employees, name='employees'),
+    path('getprofession/<int:id>', views.getprofession, name='getprofession'),
+]
