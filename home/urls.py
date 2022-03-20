fa12186e61e237b3c5187240f773689e328f3d74
NikolaJo123 <nikola_ate@hotmail.com>
2022-03-20 13:58:27 +0100

    Added routes for treatment, single patient treatment and single patient profile.

--- home/urls.py
+++ home/urls.py
@@ -10,8 +10,12 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('register/', views.register, name="register"),
     path("logout/", views.logout_view, name="logout"),
+    path("treatment/", views.treatment, name="treatment"),
 
     # API Routes
     path('patients/patient/', views.patient, name='patient'),
     path('patients/register_patient/', views.register_patient, name='register_patient'),
+    path('treatment/get_treatment/', views.get_treatment, name='get_treatment'),
+    path('treatment/get_single_treatment/<int:id>', views.get_single_treatment, name='get_single_treatment'),
+    path('treatment/get_single_patient/<int:id>', views.get_single_patient, name='get_single_patient'),
 ]
